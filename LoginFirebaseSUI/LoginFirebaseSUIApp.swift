b3eb7c48507a46d78c2a21edc88571fcceef5a2d
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-21 12:37:24 +0300

    Add Firebase

--- LoginFirebaseSUI/LoginFirebaseSUIApp.swift
+++ LoginFirebaseSUI/LoginFirebaseSUIApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct LoginFirebaseSUIApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
