230b6c1fc0de048e0337190e57d9b1a4744e1c2b
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-21 10:24:59 +0300

    Initial Commit

---
+++ LoginFirebaseSUI/LoginFirebaseSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFirebaseSUIApp.swift
+//  LoginFirebaseSUI
+//
+//  Created by Alex Kulish on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginFirebaseSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
